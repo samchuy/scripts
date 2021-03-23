1ce17a48a8adfe27d96c785686913c8051739a67
samchuy <76090012+samchuy@users.noreply.github.com>
2021-03-24 00:00:26 +0800

    Delete scripts.py

--- scripts.py
+++
@@ -1,7 +0,0 @@
-import os
-
-with open('ip_addresses.txt') as addresses:
-    for ip_Add in addresses:
-        os.system("echo Agent_Address = {}".format(ip_Add))
-       # os.system("c:/usr/bin/snmpwalk -v 2c -c public "+ip_Add+"	1.3.6.1.4.1.5961.5.1")
-        os.system("echo -------")
